b4005e2e1e69aabb007d06e02f64e7be15a3feec
pauuulaaa <heleinapaula@gmail.com>
2024-09-13 16:57:22 +0800

    Create paulamolina number.js

---
+++ paulamolina number.js
@@ -0,0 +1,21 @@
+const firstFavNum = 6;
+const secondFavNum = 9;
+//Addition
+const sum = firstFavNum + secondFavNum;
+console.log(sum);
+//Subtraction
+const difference = firstFavNum - secondFavNum;
+console.log(difference);
+//Multiplication
+const product = firstFavNum * secondFavNum;
+console.log(product);
+//Division
+const quotient = firstFavNum / secondFavNum;
+console.log(quotient);
+//Modulus
+const remainder = firstFavnum % secondFavNum;
+console.log(remainder);
+//Exponentiation(Power)
+const power = firstFavNum ** secondFavNum;
+console.log(power);
+
